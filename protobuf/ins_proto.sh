a4216ab9a10599a58f7ad6a66263e53e4fbd0766
Diego Diaz <berith.py@gmail.com>
2018-03-13 18:27:10 -0300

    protoinstaller

---
+++ protobuf/ins_proto.sh
@@ -0,0 +1,16 @@
+PROTO_VER=3.5.1
+# Make sure you grab the latest version
+curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+
+# Unzip
+unzip protoc-3.5.1-linux-x86_64.zip -d protoc3
+
+# Move protoc to /usr/local/bin/
+sudo mv protoc3/bin/* /usr/local/bin/
+
+# Move protoc3/include to /usr/local/include/
+sudo mv protoc3/include/* /usr/local/include/
+
+# Optional: change owner
+sudo chown $USER /usr/local/bin/protoc
+sudo chown -R $USER /usr/local/include/google
